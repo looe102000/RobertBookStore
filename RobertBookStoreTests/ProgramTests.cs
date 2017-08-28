edf32f3466761051ea68542728293882a87a534e
looe <looe102000@gmail.com>
2017-08-28 21:53:49 +0800

    新增測試案例 二  紅燈

--- RobertBookStoreTests/ProgramTests.cs
+++ RobertBookStoreTests/ProgramTests.cs
@@ -26,5 +26,27 @@ namespace RobertBookStore.Tests
 
             Assert.AreEqual(expected, CustomerShoppingCart.GrossPrice);
         }
+
+        [TestMethod()]
+        public void 兩本書時要要折扣()
+        {
+            //arrange
+            var target = new BookStore();
+            var CustomerShoppingCart = new ShoppingCart
+            {
+                buyBookProduct = new List<Product>
+                {
+                    new Product{ProductItem="哈利波特一",Quantity=1},
+                    new Product{ProductItem="哈利波特二",Quantity=1}
+                }
+            };
+
+            target.ShoppingCartCalculation(CustomerShoppingCart);
+
+            //assert
+            var expected = 190;
+
+            Assert.AreEqual(expected, CustomerShoppingCart.GrossPrice);
+        }
     }
 }
