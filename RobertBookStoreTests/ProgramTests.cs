144a326274015e16944e71dbd751cdfa5f307e6b
looe <looe102000@gmail.com>
2017-08-28 20:16:39 +0800

    建立專案及單元測試

---
+++ RobertBookStoreTests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RobertBookStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RobertBookStore.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GET_BOOK()
+        {
+            Assert.Fail();
+        }
+    }
+}
