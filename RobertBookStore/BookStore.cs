82e9506e79abe5f669478559a1acd4902f954dce
looe <looe102000@gmail.com>
2017-08-28 22:50:47 +0800

    一二集各買了一本_第三集買了兩本 失敗
    五個紅燈

--- RobertBookStore/BookStore.cs
+++ RobertBookStore/BookStore.cs
@@ -5,58 +5,49 @@ namespace RobertBookStore
 {
     public class BookStore
     {
-        //public List<Book> BookList { get; set; }
-
         public BookStore()
         {
-            //不做庫存檢查
-            //BookList = new List<Book>()
-            //{
-            //    new Book{ Name = "哈利波特一" , SalePrice=100 },
-            //    new Book{ Name = "哈利波特二" , SalePrice=100 },
-            //    new Book{ Name = "哈利波特三" , SalePrice=100 },
-            //    new Book{ Name = "哈利波特四" , SalePrice=100 },
-            //    new Book{ Name = "哈利波特五" , SalePrice=100 }
-            //};
-
         }
 
         public void ShoppingCartCalculation(ShoppingCart customerShoppingCart)
         {
             var total = 0d;
+            var MAX_Transaction = 0;
 
-            //將購物車內物品依序讀取
-            foreach (var ShoppingCartItem in customerShoppingCart.buyBook.OrderBy(x=>x.Name))
-            {
-                total += ShoppingCartItem.Quantity * ShoppingCartItem.SalePrice;
+            MAX_Transaction = customerShoppingCart.buyBook.Max(x => x.Quantity);
 
-                ////與庫存做比較
-                //foreach(var bookItem in BookList)
-                //{
-                //    if(ShoppingCartItem.ProductItem == bookItem.Name)
-                //    {
-                //        total += ShoppingCartItem.Quantity * bookItem.SalePrice;
-                //    }
-                //}
-            }
+            for(int i=0;i < MAX_Transaction; i++)
+            {
+                //將購物車內物品依序讀取
+                foreach (var ShoppingCartItem in customerShoppingCart.buyBook.OrderBy(x => x.Name))
+                {
+                    total += ShoppingCartItem.Quantity * ShoppingCartItem.SalePrice;
+                    ShoppingCartItem.Quantity = ShoppingCartItem.Quantity - 1;
 
-            if(customerShoppingCart.buyBook.Count == 2)
-            {
-                total = total * 0.95d;
-            }
-            else if(customerShoppingCart.buyBook.Count == 3)
-            {
-                total = total * 0.9d;
-            }
-            else if (customerShoppingCart.buyBook.Count == 4)
-            {
-                total = total * 0.8d;
-            }
-            else if (customerShoppingCart.buyBook.Count >= 5)
-            {
-                total = total * 0.75d;
-            }
+                    if (ShoppingCartItem.Quantity == 0)
+                    {
+                        customerShoppingCart.buyBook.Remove(ShoppingCartItem);
+                    }
+                }
 
+                if (customerShoppingCart.buyBook.Count == 2)
+                {
+                    total = total * 0.95d;
+                }
+                else if (customerShoppingCart.buyBook.Count == 3)
+                {
+                    total = total * 0.9d;
+                }
+                else if (customerShoppingCart.buyBook.Count == 4)
+                {
+                    total = total * 0.8d;
+                }
+                else if (customerShoppingCart.buyBook.Count >= 5)
+                {
+                    total = total * 0.75d;
+                }
+            }
+           
             customerShoppingCart.GrossPrice = total;
         }
     }
