425d65ea327760f4fabb31b3f65f8c635bdf2742
looe <looe102000@gmail.com>
2017-08-31 10:48:26 +0800

    覺得可以這樣解決 但 紅燈更多了

--- RobertBookStore/BookStore.cs
+++ RobertBookStore/BookStore.cs
@@ -14,33 +14,11 @@ namespace RobertBookStore
             var SubTotal = 0m;         //每個區塊的總額
             var BookCout = 0;          //區塊中書籍量
             var LastBookName = "";     //上一本書名
-            var SumTotlal = 0m;        //區塊總和
-            var MAX_BookQuantity = 0;   //計算區塊數量
 
-            List<Book> booklist = new List<Book>();
 
-            MAX_BookQuantity = customerShoppingCart.buyBook.Max(x => x.Quantity);
-
-            for (int i = 0; i < MAX_BookQuantity; i++)
-            {
-                //先將書籍依序讀出
-                foreach (var ShoppingCartItem in customerShoppingCart.buyBook.OrderBy(x => x.Name))
-                {
-                    //將數量減1
-                    ShoppingCartItem.Quantity = ShoppingCartItem.Quantity - 1;
-
-                    //數量為0的移除 list
-                    if (ShoppingCartItem.Quantity == 0)
-                    {
-                        customerShoppingCart.buyBook.Remove(ShoppingCartItem);
-                    }
-
-                    booklist.Add(ShoppingCartItem);
-                }
-            }
+            List<Book> booklist = GetBookList(customerShoppingCart);
 
             List<decimal> payPriceList = new List<decimal>();
-
             //先將書籍依序讀出
             foreach (var discount_item in _discount)
             {
@@ -71,67 +49,47 @@ namespace RobertBookStore
 
                     if (PayBook.index == booklist.Count - 1)
                     {
-                        payPriceSalePriceList.Add(SubTotal);
+                        payPriceSalePriceList.Add(SubTotal * GetDiscount(BookCout));
                     }
                 }
 
                 payPriceList.Add(payPriceSalePriceList.Sum());
-                
-                SumTotlal = 0;
+
                 SubTotal = 0;
                 BookCout = 0;
             }
 
-            customerShoppingCart.GrossPrice = payPriceList.Min();
+            customerShoppingCart.GrossPrice = payPriceList.Where(x=>x >0).Min();
+        }
 
+        /// <summary>
+        /// 將書順序整理
+        /// </summary>
+        /// <param name="customerShoppingCart"></param>
+        /// <returns></returns>
+        private static List<Book> GetBookList(ShoppingCart customerShoppingCart)
+        {
+            var MAX_BookQuantity = customerShoppingCart.buyBook.Max(x => x.Quantity);
+            List<Book> booklist = new List<Book>();
+            for (int i = 0; i < MAX_BookQuantity; i++)
+            {
+                //先將書籍依序讀出
+                foreach (var ShoppingCartItem in customerShoppingCart.buyBook.OrderBy(x => x.Name))
+                {
+                    //將數量減1
+                    ShoppingCartItem.Quantity = ShoppingCartItem.Quantity - 1;
 
+                    //數量為0的移除 list
+                    if (ShoppingCartItem.Quantity == 0)
+                    {
+                        customerShoppingCart.buyBook.Remove(ShoppingCartItem);
+                    }
 
+                    booklist.Add(ShoppingCartItem);
+                }
+            }
 
-
-
-
-
-            //var SubTotal = 0m;         //每個區塊的總額
-            //var MAX_Transaction = 0;   //計算區塊數量
-            //var BookCout = 0;          //區塊中書籍量
-            //var LastBookName = "";     //上一本書名
-            //var SumTotlal = 0m;        //區塊總和
-
-            //MAX_Transaction = customerShoppingCart.buyBook.Max(x => x.Quantity);
-
-            //for (int i = 0; i < MAX_Transaction; i++)
-            //{
-            //    BookCout = 0;
-            //    LastBookName = "";
-            //    SubTotal = 0m;
-
-            //    //將購物車內物品依序讀取
-            //    foreach (var ShoppingCartItem in customerShoppingCart.buyBook.OrderBy(x => x.Name))
-            //    {
-            //        //將價格依序累計
-            //        SubTotal += ShoppingCartItem.SalePrice;
-
-            //        //將數量減1
-            //        ShoppingCartItem.Quantity = ShoppingCartItem.Quantity - 1;
-
-            //        //數量為0的移除 list
-            //        if (ShoppingCartItem.Quantity == 0)
-            //        {
-            //            customerShoppingCart.buyBook.Remove(ShoppingCartItem);
-            //        }
-
-            //        //判斷 書名是否一樣
-            //        if (ShoppingCartItem.Name != LastBookName)
-            //        {
-            //            LastBookName = ShoppingCartItem.Name;
-            //            BookCout++;
-            //        }
-            //    }
-
-            //    SumTotlal += SubTotal * GetDiscount(BookCout);
-            //}
-
-            //customerShoppingCart.GrossPrice = SumTotlal;
+            return booklist;
         }
 
         private decimal GetDiscount(int bookCout)
